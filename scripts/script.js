3a4f1fa549333e5cd1eb3f3a284b9416b0c5d385
Illarion Pecheritsa <d3loff.p@yandex.ru>
2021-08-31 19:45:29 +0300

    готово

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,8 @@
-/* ADD BUTTON*/
+let name = document.querySelector('#name')
+let job = document.querySelector('#job')
+let profileNickname = document.querySelector('.profile__nickname')
+let profileProfession = document.querySelector('.profile__profession')
 
-// const addButton = document.querySelector('.profile__button-add')
-
-// function addElement() {
-//     let elements = document.querySelector('.elements')
-//     elements.insertAdjacentHTML('beforeend', `
-//     <div class="element">
-//         <img src="images/kirill-pershin-1088404-unsplash.png" class="element__image" alt="">
-//         <div class="element__footer">
-//           <h2 class="element__place-name">
-//             Карачаевск
-//           </h2>
-//           <button type="button" class="element__like">
-//            <img src="images/heart.svg" class="element__heart" alt="Кнопка нравится">
-//           </button>
-//         </div>
-//       </div>`);
-// }
-
-// addButton.addEventListener('click', addElement);
 
 /*EDITE BUTTON*/
 
@@ -27,11 +11,7 @@ const editeButton = document.querySelector('.profile__button-edite')
 
 function popupOpened() {
     popup.classList.add('popup_opened');
-    let name = document.querySelector('#name')
-    let job = document.querySelector('#job')
-    let profileNickname = document.querySelector('.profile__nickname')
-    let profileProfession = document.querySelector('.profile__profession')
-    name.value =profileNickname.textContent;
+    name.value = profileNickname.textContent;
     job.value = profileProfession.textContent;
 }
 
