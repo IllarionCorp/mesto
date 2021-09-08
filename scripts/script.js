bb2542c0f181419d2fbce542ce306e992523013f
Illarion Pecheritsa <d3loff.p@yandex.ru>
2021-09-08 20:52:22 +0300

    Revert "На ревью"
    This reverts commit 5b5172ca376d53d95afed1087f8ff070f05859f1.

--- scripts/script.js
+++ scripts/script.js
@@ -62,19 +62,14 @@ function popupClosedAdd() {
 
 closeIconAdd.addEventListener('click', popupClosedAdd);
 
-const img = document.querySelector('#image')
-const closeImgBtn = document.querySelector('#image-closed')
+function interact() {
 
-function popupOpenedImg() {
- img.classList.add('popup_opened');
+
+const i = 0;
+console.log(i);
 }
-
-function popupClosedImg() {
- img.classList.remove('popup_opened');
-}
-
-closeImgBtn.addEventListener('click', popupClosedImg);
-
+// debugger;
+// ADD POPUP FORM
 const initCards = [
  {
   name: 'Архыз',
@@ -104,7 +99,18 @@ const initCards = [
 
 const elements = document.querySelector('.elements');
 
+const img = document.querySelector('#image')
+const closeImgBtn = document.querySelector('#image-closed')
 
+function popupOpenedImg() {
+ img.classList.add('popup_opened');
+}
+
+function popupClosedImg() {
+ img.classList.remove('popup_opened');
+}
+
+closeImgBtn.addEventListener('click', popupClosedImg);
 
 const createCard = (element) => {
  const elementTemplate = document.querySelector('#card-template').content;
