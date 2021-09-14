fcc27b8f5e9cf9ae1f61f6335d3ba6de73781edf
Illarion <illarionpecheritsa@yandex.ru>
2021-09-14 16:25:25 +0300

    Валиация

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,52 +3,68 @@ enableValidation({
  inputSelector: '.fields__input',
  submitButtonSelector: '.fields__submit-button',
  inactiveButtonClass: 'fields__submit-button_disable',
- inputErrorClass: 'popup__input_type_error',
- errorClass: 'popup__error_visible'
+ inputErrorClass: 'fields__input_type_error',
+ errorClass: 'fields__input-error'
 });
 
+
+// const showInputError = (formElement, inputElement, errorMessage) => {
+//  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+//  inputElement.classList.add('fields__input_type_error');
+//  errorElement.textContent = errorMessage;
+//  errorElement.classList.add('fields__input-error');
+// }
+
+const enableValidation = (enableValidation) => {
+ // const fieldsList= Array.from(document.querySelectorAll(enableValidation.formSelector));
+ console.log(enableValidation.formSelector);
+}
+
+enableValidation(enableValidation);
+
+
 //объявляем фукнцию валидации добавляем ей объекты, которые она обрабатывает
-const enableValidation = (objectsOfValidation) => {
-const formList = Array.from(objectsOfValidation);
-//каждой форме добавляем слушатель
-formList.forEach(setEventListeners);
-}
+// const enableValidation = (objectsOfValidation) => {
+// const formList = Array.from(objectsOfValidation);
+// //каждой форме добавляем слушатель
+// formList.forEach(setEventListeners);
+// }
 
 
-//показ ошибки
-const showInputError = (formSelector, inputSelector, inputErrorClass, errorClass) => {
-inputErrorClass = formSelector.querySelector(`.${inputSelector.id}-error`);
-inputSelector.classList.add('name-input-error');
-inputErrorClass.textContent = errorClass;
-inputErrorClass.classList.add('job-input-error');
-};
+// //показ ошибки
+// const showInputError = (formSelector, inputSelector, inputErrorClass, errorClass) => {
+// inputErrorClass = formSelector.querySelector(`.${inputSelector.id}-error`);
+// inputSelector.classList.add('name-input-error');
+// inputErrorClass.textContent = errorClass;
+// inputErrorClass.classList.add('job-input-error');
+// };
 
-// скрытие ошибки
-const hideInputError = (formSelector, inputSelector) => {
-inputErrorClass = formSelector.querySelector(`.${inputSelector.id}-error`);
-inputSelector.classList.remove('name-input-error');
-inputErrorClass.classList.remove('job-input-error');
-inputErrorClass.textContent = '';
-};
+// // скрытие ошибки
+// const hideInputError = (formSelector, inputSelector) => {
+// inputErrorClass = formSelector.querySelector(`.${inputSelector.id}-error`);
+// inputSelector.classList.remove('name-input-error');
+// inputErrorClass.classList.remove('job-input-error');
+// inputErrorClass.textContent = '';
+// };
 
-//функция проверки на валидность
-const isValid = (formSelector, inputSelector, errorClass) => {
-if (!inputSelector.validity.valid) {
- showInputError(formSelector, inputSelector, inputSelector.validationMessage, errorClass);
-} else {
- hideInputError(formSelector, inputSelector, errorClass);
-}
-};
-// описываем слушатели
-function setEventListeners(formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, errorClass) {
-const inputsAll = Array.from(document.querySelectorAll(inputSelector))
-const buttonSubmit = formSelector.querySelector(submitButtonSelector)
-inputsAll.forEach(input =>{
- input.addEventListener('input',() =>{
- isValid(formSelector, inputSelector, errorClass)
+// //функция проверки на валидность
+// const isValid = (formSelector, inputSelector, errorClass) => {
+// if (!inputSelector.validity.valid) {
+//  showInputError(formSelector, inputSelector, inputSelector.validationMessage, errorClass);
+// } else {
+//  hideInputError(formSelector, inputSelector, errorClass);
+// }
+// };
+// // описываем слушатели
+// function setEventListeners(formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, errorClass) {
+// const inputsAll = Array.from(document.querySelectorAll(inputSelector))
+// const buttonSubmit = formSelector.querySelector(submitButtonSelector)
+// inputsAll.forEach(input =>{
+//  input.addEventListener('input',() =>{
+//  isValid(formSelector, inputSelector, errorClass)
 
-})
-})
-}
+// })
+// })
+// }
 
-enableValidation(objectsOfValidation);
+// enableValidation(objectsOfValidation);
