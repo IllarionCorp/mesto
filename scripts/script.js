e5539037ac0f3e5027d659c6f2570cfc0ed72538
Illarion <illarionpecheritsa@yandex.ru>
2021-09-15 15:43:28 +0300

    Закрытие попапа

--- scripts/script.js
+++ scripts/script.js
@@ -145,10 +145,20 @@ closePopup(imgPopup);
 PopupAddImage.addEventListener('submit', formSubmitHandlerAdd);
 
 popupArr.forEach((evt) => {
+ const popupContainer = evt.querySelector('.popup__container');
  evt.addEventListener('keydown', function(e) {
+  console.log(e);
   if (e.key === 'Escape') {
-   closePopup(document.querySelector(`#${evt.id}`));
+   closePopup(evt);
   }
+  evt.addEventListener('click', function(e) {
+  if (e !== document.classList.content('popup')) {
+    closePopup(evt);
+    console.log('1');
+   }
+  });
  });
-});
+ });
+
+
 
